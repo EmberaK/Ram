fc248b63675bbc88005c5d93cbde309814a0d3fc
EmberaK <vlasukula11@gmail.com>
2024-09-26 19:03:24 +0300

    added update codes for 'module1-task1'

--- js/main.js
+++ js/main.js
@@ -3,28 +3,31 @@ import { getRandomNumber, getRandomElement } from './utils.js';
 
 // Генерує коментар
 function createComment() {
-    return {
-        id: getRandomNumber(userIdRange.min, userIdRange.max),
-        name: getRandomElement(userNames),
-        avatar: `img/avatar-${getRandomNumber(avatarRange.min, avatarRange.max)}.svg`,
-        message: getRandomElement(feedbackMessages),
-    };
+  return {
+    id: getRandomNumber(userIdRange.min, userIdRange.max),
+    name: getRandomElement(userNames),
+    avatar: `img/avatar-${getRandomNumber(avatarRange.min, avatarRange.max)}.svg`,
+    message: getRandomElement(feedbackMessages),
+  };
 }
 
 // Генерує масив випадкових коментарів
 function createCommentsArray() {
-    const quantityComments = getRandomNumber(commentRange.min, commentRange.max);
-    return Array.from({ length: quantityComments }, createComment);
+  const quantityComments = getRandomNumber(commentRange.min, commentRange.max);
+  return Array.from({ length: quantityComments }, createComment);
 }
 
 // Оновлюємо масив фотографій, додаючи властивості до кожного об'єкта
-const updatedPublicsArray = publicsArray.map((_, index) => ({
+const updatedPublicsArray = publicsArray.map(function(_, index) {
+  return {
     id: index + 1,
     url: `photos/${index + 1}.jpg`,
     description: `photo description ${index + 1}`,
     likes: getRandomNumber(likeRange.min, likeRange.max),
     comments: createCommentsArray(),
-}));
+  };
+});
+
 
 // Лог для перевірки отриманого масиву фотографій з коментарями
 console.log(updatedPublicsArray);
